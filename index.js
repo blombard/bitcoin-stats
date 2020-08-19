e3b930d0f0dcc34f5e0d13418e40161e86a7c16b
Baptiste Lombard <btlcasio@gmail.com>
2020-08-19 19:16:26 +0200

    Use async/await

--- index.js
+++ index.js
@@ -13,43 +13,42 @@ const d3 = require('d3');
 const techan = require('techan');
 const chart = require('./chart.js');
 
-axios.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=100')
-  .then(data => {
-    const csvData = data.data.map(d => {
-      return {
-        Date: moment(d[0]).format('DD-MMM-YY'),
-        Open: d[1],
-        High: d[2],
-        Low: d[3],
-        Close: d[4],
-        Volume: d[5]
-      }
-    })
+async function run() {
+  const { data } = await axios.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=100');
+  const csvData = data.map(d => {
+    return {
+      Date: moment(d[0]).format('DD-MMM-YY'),
+      Open: d[1],
+      High: d[2],
+      Low: d[3],
+      Close: d[4],
+      Volume: d[5]
+    };
+  });
     
-    const { document } = (new JSDOM('')).window;
-    // Create the chart, passing in runtime environment specific setup: node d3, techan and csv data
-    const body = d3.select(document.body).call(chart(d3, techan, csvData));
+  const { document } = (new JSDOM('')).window;
+  // Create the chart, passing in runtime environment specific setup: node d3, techan and csv data
+  const body = d3.select(document.body).call(chart(d3, techan, csvData));
 
-    var stream = fs.createWriteStream("chart.svg");
-    stream.once('open', function(fd) {
-      stream.write('<?xml version="1.0" encoding="utf-8"?>\n');
-      stream.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n');
-      stream.write(body.html());
-      stream.end();
-    });
+  var stream = fs.createWriteStream("chart.svg");
+  stream.once('open', function(fd) {
+    stream.write('<?xml version="1.0" encoding="utf-8"?>\n');
+    stream.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n');
+    stream.write(body.html());
+    stream.end();
+  });
 
-    axios.get('https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT')
-      .then(data => {
-        const { priceChangePercent, lastPrice, highPrice, lowPrice } = data.data;
-        const stats = `|$${parseFloat(lastPrice).toFixed(2)}|${parseFloat(priceChangePercent).toFixed(2)}%|$${parseFloat(highPrice).toFixed(2)}|$${parseFloat(lowPrice).toFixed(2)}|\n\n`;
-        var stream = fs.createWriteStream("README.md");
-        stream.once('open', function(fd) {
-          stream.write('# Bitcoin Stats\n\n');
-          stream.write('BTC/USDT|24h change|24h high|24h low|\n');
-          stream.write('|---|---|---|---|\n');
-          stream.write(stats);
-          stream.write('<img src="./chart.svg">\n');
-          stream.end();
-        });
-      })
-  })
+  const { data: { priceChangePercent, lastPrice, highPrice, lowPrice } } = await axios.get('https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT');
+  const stats = `|$${parseFloat(lastPrice).toFixed(2)}|${parseFloat(priceChangePercent).toFixed(2)}%|$${parseFloat(highPrice).toFixed(2)}|$${parseFloat(lowPrice).toFixed(2)}|\n\n`;
+  var stream = fs.createWriteStream("README.md");
+  stream.once('open', function(fd) {
+    stream.write('# Bitcoin Stats\n\n');
+    stream.write('BTC/USDT|24h change|24h high|24h low|\n');
+    stream.write('|---|---|---|---|\n');
+    stream.write(stats);
+    stream.write('<img src="./chart.svg">\n');
+    stream.end();
+  });
+};
+
+run();
